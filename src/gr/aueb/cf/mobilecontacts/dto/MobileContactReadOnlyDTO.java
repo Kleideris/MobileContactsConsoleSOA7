4454ebf748bcb2ab964c70b782ccef81bd22640d
Ch. Kleideris <kleiderisch@gmail.com>
2025-01-29 17:26:12 +0200

    Deleted setters from ReadOnlyDTO

--- src/gr/aueb/cf/mobilecontacts/dto/MobileContactReadOnlyDTO.java
+++ src/gr/aueb/cf/mobilecontacts/dto/MobileContactReadOnlyDTO.java
@@ -20,23 +20,11 @@ public class MobileContactReadOnlyDTO extends BaseDTO {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
     public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 }
